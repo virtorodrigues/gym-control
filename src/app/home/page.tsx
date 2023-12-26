e5a16ff2d89c75f5d1ad836289f888e852bea50d
Vitor Rodrigues <vitor@brasilbybus.com>
2023-12-26 14:51:05 -0300

    feat: improvement login page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -5,40 +5,50 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-      <h1 className='text-5xl mb-7'>Login</h1>
-      <Form.Root className="flex flex-col space-y-5 w-72">
-        <Form.Field className="flex flex-col space-y-1" name="email">
-          <div className="flex items-baseline justify-between">
-            <Form.Label className="font-medium text-white">Email</Form.Label>
+      <div className='bg-white p-16 rounded-md text-center border-gray-200 border'>
+        <h1 className='text-3xl mb-7'>Acessar conta</h1>
+        <Form.Root className="flex flex-col space-y-6 w-80">
+          <div className='flex flex-col space-y-6'>
+            <Form.Field className="flex flex-col space-y-1" name="email">
+              <div className="flex items-baseline justify-between">
+                <Form.Label className="text-gray-400 text-sm">Email</Form.Label>
+              </div>
+              <Form.Control asChild>
+                <input
+                  className="py-2 px-3 text-slate-950 rounded-md border border-gray-200"
+                  type="email"
+                  placeholder='digite seu email...'
+                  required
+                />
+              </Form.Control>
+            </Form.Field>
+            <Form.Field className="flex flex-col space-y-1" name="email">
+              <div className="flex items-baseline justify-between">
+                <Form.Label className="text-gray-400 text-sm">Senha</Form.Label>
+              </div>
+              <Form.Control asChild>
+                <input
+                  className="py-2 px-3 text-slate-950 rounded-md border border-gray-200"
+                  type="password"
+                  placeholder='digite sua senha...'
+                  required
+                />
+              </Form.Control>
+            </Form.Field>
           </div>
-          <Form.Control asChild>
-            <input
-              className="py-2 px-3 text-slate-950 rounded-md"
-              type="email"
-              placeholder='digite seu email...'
-              required
-            />
-          </Form.Control>
-        </Form.Field>
-        <Form.Field className="flex flex-col space-y-1" name="email">
-          <div className="flex items-baseline justify-between">
-            <Form.Label className="font-medium text-white">Senha</Form.Label>
+          <div>
+            <div className="text-end">
+              <a href='#' className="text-gray-400 text-sm hover:text-orange-700 duration-300">esqueci minha senha</a>
+            </div>
+            <Form.Submit asChild>
+              <button className="mt-4 hover:brightness-90 duration-300 w-full px-4 py-2 bg-orange-700 text-gray-200 font-semibold tracking-wider rounded-md">
+                Entrar
+              </button>
+            </Form.Submit>
           </div>
-          <Form.Control asChild>
-            <input
-              className="py-2 px-3 text-slate-950 rounded-md"
-              type="password"
-              placeholder='digite sua senha...'
-              required
-            />
-          </Form.Control>
-        </Form.Field>
-        <Form.Submit asChild>
-          <button className="mt-4 hover:brightness-90 duration-300 w-full p-4 bg-orange-700 text-gray-200 font-semibold tracking-wider rounded-md">
-            Entrar
-          </button>
-        </Form.Submit>
-      </Form.Root>
+        </Form.Root>
+      </div>
+
     </main>
   )
 }
